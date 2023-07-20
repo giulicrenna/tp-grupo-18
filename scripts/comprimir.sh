95abde5960f27a2e7e6ae0445ccdd36b0a880c4a
Rocío Hachen <rociohachen@hotmail.com>
2023-07-20 10:32:50 -0300

    Actualización comprimir.sh

--- scripts/comprimir.sh
+++ scripts/comprimir.sh
@@ -45,11 +45,22 @@ function comprimir {
 
 	# Entiendase por nombre valido todos los nombres donde la primer letra este en mayuscula y el resto en minuscula, antes y despues del _ 
 	# En el dir IMAGENES_VALIDAS ya estan todas las imagenes con nombres validos 
+	VALIDAS=0
 	for IMAGEN in $IMAGENES_VALIDAS
 	do
+		let VALIDAS++
 		NOMBRE_SIN_EXTENSION=$(basename $IMAGEN .jpg)
 		echo $NOMBRE_SIN_EXTENSION >> Nombre_imagenes_validas
 	done 
+	
+	#En caso de que no encuentre imagenes válidas, no intenta comprimir el archivo. 
+	if [[ VALIDAS -ne 0 ]]
+	then 
+		zip archivos_comprimidos Nombres_imagenes_validas
+		rm Nombres_imagenes_validas
+	else
+		echo "No se encontraron imágenes con nombres válidos."
+	fi
 
 	# Para conseguir los nombres que terminen con a 
 	CONTADOR=0    #Para llevar la cuenta de los nombres que terminan con a
@@ -70,11 +81,11 @@ function comprimir {
         fi 
 
 	# Comprimir los archivos generados en un archivo llamado Archivos_comprimidos.zip
-	zip archivos_comprimidos Nombre_imagenes Nombre_imagenes_validas Nombres_terminados_en_a
+	zip archivos_comprimidos Nombre_imagenes Nombres_terminados_en_a
 
 	#Quitamos los archivos ya comprimidos.
 	rm Nombre_imagenes 
-	rm Nombre_imagenes_validas 
+	#rm Nombre_imagenes_validas 
 	rm Nombres_terminados_en_a
 
 	echo "Comprimiendo imágenes" #Agregamos las imágenes al archivo comprimido
